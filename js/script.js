b4ed1bc278f8d90a4a660d007d388a3f9865bec3
eslam shosha <you@example.comeslamshosha22.es@gmail.com>
2022-02-13 11:35:25 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+  new WOW().init();
+
+  //phone size menu onclick
+  if ($(window).width() <= 1199) {
+    $("#menu-id").click(function (e) {
+      e.preventDefault();
+      $(".overlay-box").fadeToggle(300);
+      $(".navgition").toggleClass("reset-left");
+      $("body").toggleClass("overflow");
+    });
+    $(".nav-head .close-btn, .overlay-box").click(function () {
+      $(".overlay-box").fadeOut(300);
+      $(".navgition").removeClass("reset-left");
+      $(".menu-bars .bars").toggleClass("open-bars");
+      $(".menu-bars .bars").toggleClass("close-bars");
+      $("body").removeClass("overflow");
+    });
+  }
+  $("#menu-id").click(function () {
+    $(".menu-bars .bars").toggleClass("open-bars");
+    $(".menu-bars .bars").toggleClass("close-bars");
+  });
+  //fixed nav
+  $stickyNav = $(".top-header");
+  $(window).on("scroll load", function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 120) {
+      $stickyNav.addClass("fixed-nav", 500);
+    } else {
+      $stickyNav.removeClass("fixed-nav", 500);
+    }
+    if (scroll == 0) {
+      $stickyNav.removeClass("fixed-nav", 500);
+    }
+  });
+  //scroll down button to about us section
+	$("#scroll-to-about-section").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#about-us").offset().top
+        }, 1000);
+    });
+});
